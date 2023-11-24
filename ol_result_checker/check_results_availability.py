89a173d8cb8e405fca5845c8310febea9e9ef225
Sahan Yashmika <88578622+SahanY099@users.noreply.github.com>
2023-11-24 14:57:58 +0530

    Fix the bug in checking result availability

--- ol_result_checker/check_results_availability.py
+++ ol_result_checker/check_results_availability.py
@@ -11,12 +11,7 @@ def is_results_available() -> bool:
     response = requests.get(RESULT_PRESENT_CHECKER_API_ENDPOINT)
     data = response.json()
 
-    # Check if "2022" or "2022/2023" is a key
-    if "2022" in data.keys() or "2022/2023" in data.keys():
-        return True
-
-    # Check if "2022" or "2022/2023" is a value
-    if "2022" in data.values() or "2022/2023" in data.values():
+    if data["yearOlResult"] == "2022":
         return True
 
     return False
